1004a275f231792bb4d0dac089f49f900202b499
unknown <aleksandarristov28@gmail.com>
2023-11-24 20:28:24 +0100

    first commit

---
+++ Test.java
@@ -0,0 +1,18 @@
+public class Test{
+
+	public static void main(String[] args) {
+		
+		
+		class MyClass {
+			
+			static void soberiBroja() {
+				
+				double a=2,b=3,c;
+				c=a+b;
+				System.out.println(c);		
+			}
+			
+		}
+		
+	}
+}
